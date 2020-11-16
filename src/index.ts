e5c634ca55fffef0e1cdbfef95064a2251dd647a
Thibault RICHARD <thibault@widop.com>
2020-11-16 14:47:37 +0100

    [#3] Goodbye var

--- src/index.ts
+++ src/index.ts
@@ -78,9 +78,10 @@ class Index extends HTMLElement {
     str = str.replace(/^\s+|\s+$/g, ''); // trim
     str = str.toLowerCase();
 
-    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-    var to   = "aaaaeeeeiiiioooouuuunc------";
-    for (var i=0, l=from.length ; i<l ; i++) {
+    const from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+    const to   = "aaaaeeeeiiiioooouuuunc------";
+
+    for (let i=0, l=from.length ; i<l ; i++) {
       str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
     }
 
